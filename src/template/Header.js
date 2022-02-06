81a9f12400d8dbe5bb20047bbfa285460c4570d2
ariesplat <slcgiraldo@gmail.com>
2022-02-06 11:40:56 -0300

    carpetas2

---
+++ src/template/Header.js
@@ -0,0 +1,20 @@
+const Header = () => {
+    const view = `
+    <div class="Hearder-main">
+        <div class="Header-logo">
+            <h1>
+                <a href="/">
+                    100tifico
+                </a>
+            </h1>
+        </div>
+        <div class="Header-nav">
+            <a href="#/about/">
+            About
+            </a>
+        </div>
+    </div>
+    `;
+    return view;
+};
+export default Header;
